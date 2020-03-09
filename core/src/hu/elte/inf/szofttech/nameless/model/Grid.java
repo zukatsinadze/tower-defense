008a21f84af495beef8cba48a5b916ec81e8aa85
Xie Zong Pu <rlknma@INF.ELTE.HU>
2020-03-09 16:50:26 +0100

    Square and Grid

--- core/src/hu/elte/inf/szofttech/nameless/model/Grid.java
+++ core/src/hu/elte/inf/szofttech/nameless/model/Grid.java
@@ -1,4 +1,32 @@
 package hu.elte.inf.szofttech.nameless.model;
 
-public class Grid {
+import com.badlogic.gdx.math.GridPoint2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class Grid {
+    private final int width;
+    private final int height;
+    private final List<List<Square>> grid;
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.grid = IntStream.range(0, height)
+                .mapToObj(i -> IntStream.range(0, height)
+                        .mapToObj(j -> new Square())
+                        .collect(Collectors.toCollection(() -> new ArrayList<>(height))))
+                .collect(Collectors.toCollection((() -> new ArrayList<>(width))));
+    }
+
+    public Square getSquare(int x, int y) {
+        return grid.get(y).get(x);
+    }
+
+    public Square getSquare(GridPoint2 point) {
+        return getSquare(point.x, point.y);
+    }
 }
--- core/src/hu/elte/inf/szofttech/nameless/model/Square.java
+++ core/src/hu/elte/inf/szofttech/nameless/model/Square.java
@@ -1,4 +1,25 @@
 package hu.elte.inf.szofttech.nameless.model;
 
-public class Square {
+import hu.elte.inf.szofttech.nameless.model.tower.Tower;
+
+import java.util.Optional;
+
+public final class Square {
+    private Optional<Tower> tower;
+
+    public Square() {
+        this.tower = Optional.empty();
+    }
+
+    public Optional<Tower> getTower() {
+        return this.tower;
+    }
+
+    public void buildTower(Tower tower) {
+        this.tower = Optional.of(tower);
+    }
+
+    public void destroyTower() {
+        this.tower = Optional.empty();
+    }
 }
