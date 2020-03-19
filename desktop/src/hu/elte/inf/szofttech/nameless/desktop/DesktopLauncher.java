ccbd6f190d9155627513622dd381921ca40085ba
Szumi Xie <szumixie@gmail.com>
2020-03-19 16:48:39 +0100

    rename Main class

--- desktop/src/hu/elte/inf/szofttech/nameless/desktop/DesktopLauncher.java
+++ desktop/src/hu/elte/inf/szofttech/nameless/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package hu.elte.inf.szofttech.nameless.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import hu.elte.inf.szofttech.nameless.Game;
+import hu.elte.inf.szofttech.nameless.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }
