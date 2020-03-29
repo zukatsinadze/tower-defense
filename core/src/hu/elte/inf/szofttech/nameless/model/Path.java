3ef8e0aef978207901fec714de7c997310300c32
Szumi Xie <szumixie@gmail.com>
2020-03-29 20:01:35 +0200

    Path builder and utilities

--- core/src/hu/elte/inf/szofttech/nameless/model/Path.java
+++ core/src/hu/elte/inf/szofttech/nameless/model/Path.java
@@ -1,8 +1,8 @@
 package hu.elte.inf.szofttech.nameless.model;
 
 import com.badlogic.gdx.math.GridPoint2;
-import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -24,7 +24,36 @@ public final class Path {
         return this.points.get(index);
     }
 
+    public GridPoint2 getFirst() {
+        return this.get(0);
+    }
+
+    public GridPoint2 getLast() {
+        return this.get(this.length() - 1);
+    }
+
     public boolean onPath(GridPoint2 point) {
         return this.pointSet.contains(point);
+    }
+
+    public static class Builder {
+        private final List<GridPoint2> points;
+
+        public Builder() {
+            this.points = new ArrayList<>();
+        }
+
+        public Path build() {
+            return new Path(this.points);
+        }
+
+        public Builder add(GridPoint2 point) {
+            this.points.add(point);
+            return this;
+        }
+
+        public Builder add(int x, int y) {
+            return this.add(new GridPoint2(x, y));
+        }
     }
 }
