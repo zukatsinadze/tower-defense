cb8b768a733a376e2a69a60f42754ed498d44153
Szumi Xie <szumixie@gmail.com>
2020-04-12 23:10:06 +0200

    Delete unused font

--- core/src/hu/elte/inf/szofttech/nameless/Main.java
+++ core/src/hu/elte/inf/szofttech/nameless/Main.java
@@ -1,24 +1,24 @@
 package hu.elte.inf.szofttech.nameless;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import hu.elte.inf.szofttech.nameless.view.GameScreen;
 import hu.elte.inf.szofttech.nameless.view.MainMenuScreen;
 
 public final class Main extends Game {
     private SpriteBatch batch;
-    private BitmapFont font;
     private Viewport viewport;
     private OrthographicCamera camera;
 
-    public Viewport getViewport() { return viewport; }
-    public OrthographicCamera getCamera() { return camera; }
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    public OrthographicCamera getCamera() {
+        return camera;
+    }
 
     @Override
     public void create() {
@@ -26,7 +26,6 @@ public final class Main extends Game {
         this.camera = new OrthographicCamera();
         this.camera.setToOrtho(false, Config.screenWidth, Config.screenHeight);
         batch = new SpriteBatch();
-        font = new BitmapFont();
         this.setScreen(new MainMenuScreen(this));
     }
 
@@ -42,17 +41,10 @@ public final class Main extends Game {
 
     /**
      * for drawing
+     *
      * @return batch
      */
     public SpriteBatch getBatch() {
         return this.batch;
-    }
-
-    /**
-     * write text one the screen
-     * @return font
-     */
-    public BitmapFont getFont() {
-        return this.font;
     }
 }
