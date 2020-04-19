124adb85679c33767a5a559834f0d440fb1e730d
Szumi Xie <szumixie@gmail.com>
2020-04-19 15:21:33 +0200

    Change GUI bar size

--- core/src/hu/elte/inf/szofttech/nameless/Config.java
+++ core/src/hu/elte/inf/szofttech/nameless/Config.java
@@ -6,15 +6,15 @@ import com.badlogic.gdx.graphics.Color;
  * colors for use
  */
 public class Config {
-    public static final int tileSize = 70;
+    public static final int tileSize = 73;
     public static final int gridWidth = 13;
     public static final int gridHeight = 7;
     public static final int screenWidth = 1120;
     public static final int screenHeight = 630;
 
-    public static final int guiX = 840;
-    public static final int guiWidth = 280;
-    public static final int guiHeight = 140;
+    public static final int guiX = tileSize * gridWidth;
+    public static final int guiWidth = screenWidth - guiX;
+    public static final int guiHeight = screenHeight - tileSize * gridHeight;
 
     public static Color white = new Color(1f, 1f, 1f, .5f);
     public static Color green = new Color(.5f, 1, .5f, 1f); //slowed ailment
