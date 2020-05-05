0e793bfdbae1f4e63b4d14a089d032911113cab6
Szumi Xie <szumixie@gmail.com>
2020-05-05 16:39:09 +0200

    Fix tower textures

--- core/src/hu/elte/inf/szofttech/nameless/model/tower/TowerType.java
+++ core/src/hu/elte/inf/szofttech/nameless/model/tower/TowerType.java
@@ -5,14 +5,14 @@ import hu.elte.inf.szofttech.nameless.Textures;
 
 public enum TowerType {
     Basic1(Textures.basic1, 0, 75, 10, 10, 10, new NoSpecialAbility()),
-    Basic2(Textures.basic1, 0, 100, 12, 15, 8, new NoSpecialAbility()),
-    Basic3(Textures.basic1, 0, 125, 8, 8, 20, new NoSpecialAbility()),
-    AdvancedSlow(Textures.basic1, 15, 100, 20, 10, 12, new SlowBalloons()),
-    AdvancedFreeze(Textures.basic1, 20, 125, 10, 20, 10, new FreezeBalloons()),
-    AdvancedTeleport(Textures.basic1, 20, 125, 18, 15, 12, new Teleport()),
-    AdvancedExplosion(Textures.basic1, 25, 150, 12, 18, 8, new Explosion()),
-    AdvancedPoison(Textures.basic1, 25, 150, 15, 10, 20, new CatchFire()),
-    AdvancedFire(Textures.basic1, 30, 175, 8, 8, 30, new RingOfFire());
+    Basic2(Textures.basic2, 0, 100, 12, 15, 8, new NoSpecialAbility()),
+    Basic3(Textures.basic3, 0, 125, 8, 8, 20, new NoSpecialAbility()),
+    AdvancedSlow(Textures.advanced11, 15, 100, 20, 10, 12, new SlowBalloons()),
+    AdvancedFreeze(Textures.advanced12, 20, 125, 10, 20, 10, new FreezeBalloons()),
+    AdvancedTeleport(Textures.advanced21, 20, 125, 18, 15, 12, new Teleport()),
+    AdvancedExplosion(Textures.advanced22, 25, 150, 12, 18, 8, new Explosion()),
+    AdvancedPoison(Textures.advanced31, 25, 150, 15, 10, 20, new CatchFire()),
+    AdvancedFire(Textures.advanced32, 30, 175, 8, 8, 30, new RingOfFire());
 
     public final Texture texture;
     public final int xp;
